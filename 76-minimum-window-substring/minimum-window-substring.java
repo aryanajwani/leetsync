2b0418735dc81511d69f3128eb02d9f77ffb21ec
Aryan Ajwani <aryan.ajwani@gmail.com>
2025-12-10 21:35:13 +0530

    Time: 2 ms (99.89%) | Memory: 46.2 MB (46.15%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,18 +1,22 @@
 class Solution {
     public String minWindow(String str, String t) {
+        if(str.length()< t.length())return "";
+
+        char strChar[] = str.toCharArray();
+        char tChar[] = t.toCharArray();
+
         int hash[] = new int[256];
         for(int i=0; i<t.length(); i++){
             hash[t.charAt(i)]++;
         }
-
 
         int left=0, right=0;
         int startIndex =-1;
         int minLength = Integer.MAX_VALUE;
         int count=0;
 
-        while(right <str.length()){
-            char chr = str.charAt(right);
+        while(right <strChar.length){
+            char chr = strChar[right];
 
             if(hash[chr] >0) count++;
             hash[chr]--;
@@ -24,8 +28,8 @@ class Solution {
                     startIndex = left;
                 }
                 
-                hash[str.charAt(left)]++;
-                if (hash[str.charAt(left)] >0) count--;
+                hash[strChar[left]]++;
+                if (hash[strChar[left]] >0) count--;
                 left++;
             }
 
