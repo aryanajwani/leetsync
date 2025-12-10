dcfe78aea85d6557a002285b949758db59edd4b0
Aryan Ajwani <aryan.ajwani@gmail.com>
2025-12-11 05:19:07 +0530

    Time: 8 ms (97.86%) | Memory: 46.4 MB (16.08%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -1,22 +1,22 @@
 class Solution {
     public int numberOfSubstrings(String s) {
         char str[] = s.toCharArray();
-        int countA=1, countB=1, countC=1;
+        int countA=0, countB=0, countC=0;
         int count =0;
 
         int right=0, left=0;
 
         while(right< str.length){
-            if(str[right]=='a')  countA--;
-            else if(str[right] == 'b')  countB--;
-            else if(str[right] == 'c')  countC--;
+            if(str[right]=='a')  countA++;
+            else if(str[right] == 'b')  countB++;
+            else if(str[right] == 'c')  countC++;
             
-            while(countA<=0 && countB<=0 && countC<=0){
+            while(countA>=1 && countB>=1 && countC>=1){
                 count+= str.length-right;
 
-                if(str[left]=='a')  countA++;
-                else if(str[left] == 'b')  countB++;
-                else if(str[left] == 'c')  countC++;
+                if(str[left]=='a')  countA--;
+                else if(str[left] == 'b')  countB--;
+                else if(str[left] == 'c')  countC--;
 
                 left++;
             }
