2571fa40ad9283ec185815c9942d8108852f3f96
Aryan Ajwani <aryan.ajwani@gmail.com>
2025-12-13 18:48:02 +0530

    Time: 15 ms (45.50%) | Memory: 51.2 MB (64.21%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int student[] = g.clone();
+         int cookie[] = s.clone();
+         Arrays.sort(student);
+         Arrays.sort(cookie);
+
+         int i=0, j=0;
+         while(i<student.length && j<cookie.length){
+            if(cookie[j] >= student[i]){
+                i++;
+            }
+            j++;
+         }
+
+         return i;
+    }
+}
