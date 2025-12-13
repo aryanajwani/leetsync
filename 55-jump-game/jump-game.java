d6a17035add75f4e837d6fa0d42ed3d3212a8db7
Aryan Ajwani <aryan.ajwani@gmail.com>
2025-12-14 03:44:04 +0530

    Time: 2 ms (84.34%) | Memory: 47.9 MB (38.01%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,14 +1,32 @@
 class Solution {
-    public boolean canJump(int[] arr) {
-        int maxAllowed =0;
-        for (int i=0; i<arr.length; i++){
-            if (i > maxAllowed) return false;
-            
-            maxAllowed = Math.max(maxAllowed, i+arr[i]);
-            if (maxAllowed >= arr.length-1) return true;
-            
+    public boolean canJump(int[] nums) {
+        // Initialize maximum index
+        int maxIndex = 0;
+
+        // Iterate through each index of the array
+        for (int i = 0; i < nums.length; i++) {
+            /* If the current index 
+               is greater than the 
+               maximum reachable index
+               it means we cannot move 
+               forward and should 
+               return false */
+            if (i > maxIndex) {
+                return false;
+            }
+
+            /* Update the maximum index that can be 
+               reached by comparing
+               the current maxIndex with the sum 
+               of the current index and
+               the maximum jump from that index */
+            maxIndex = Math.max(maxIndex, i + nums[i]);
         }
 
-        return false;
+        /* If we complete the 
+           loop, it means we 
+           can reach the 
+           last index */
+        return true;
     }
 }
