0715d831c05684f65ed45bb8b4aa9e0f62523e6c
Aryan Ajwani <aryan.ajwani@gmail.com>
2025-12-20 18:16:46 +0530

    Time: 3 ms (87.64%) | Memory: 47.2 MB (75.13%) - LeetSync

---
+++ 135-candy/candy.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int total = 1;
+        int prev = 1;
+
+        int i = 1;
+
+        while (i < ratings.length) {
+            // increasing slope
+            if (ratings[i - 1] < ratings[i]) {
+                total += ++prev;
+            }
+
+            else if (ratings[i - 1] == ratings[i]) {
+                total += 1;
+                prev = 1;
+            }
+
+            // decreasing slope
+            else if (ratings[i - 1] > ratings[i]) {
+                int peak = prev;
+
+                int count = 1;
+                while (i < ratings.length && ratings[i - 1] > ratings[i]) {
+                    total += count;
+                    count++;
+                    i++;
+                }
+
+                if (count > peak)
+                    total += (count - peak);
+                prev = 1;
+                continue;
+            }
+
+            i++;
+        }
+
+        return total;
+    }
+}
