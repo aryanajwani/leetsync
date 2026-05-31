3817680753d050bbb076b91d2b7b1cc5972b9160
Aryan Ajwani <aryan.ajwani@gmail.com>
2026-06-01 03:03:31 +0530

    Time: 6 ms (99.53%) | Memory: 110.1 MB (6.12%) - LeetSync

--- 2245-destroying-asteroids/destroying-asteroids.java
+++ 2245-destroying-asteroids/destroying-asteroids.java
@@ -1,13 +1,25 @@
 class Solution {
     public boolean asteroidsDestroyed(int m, int[] asteroids) {
-        Arrays.sort(asteroids);
+        boolean found;
         long mass = m;
 
-        for(int asteroid : asteroids){
-            if(asteroid <= mass) mass+= asteroid;
-            else return false;
-        }
+        int visited[] = new int[asteroids.length];
 
+        do{
+            found = false;
+            for(int i=0; i<asteroids.length; i++){
+                if(visited[i]!= 1 && asteroids[i] <= mass){
+                    mass += asteroids[i];
+                    visited[i] = 1;
+                    found = true;
+                }
+            }
+        } while(found);
+
+
+        for(int i=0; i<visited.length; i++){
+            if(visited[i] !=1) return false;
+        }
         return true;
     }
 }
