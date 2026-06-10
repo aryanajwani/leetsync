654afeb1b2e72eb54f316b79ba91c976b329e3b9
Aryan Ajwani <aryan.ajwani@gmail.com>
2026-06-11 01:15:22 +0530

    Time: 70 ms (45.57%) | Memory: 87.1 MB (18.21%) - LeetSync

--- 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.java
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.java
@@ -19,23 +19,40 @@ class Solution {
         Map<Integer, TreeNode> map = new HashMap<>();
         Set<Integer> children = new HashSet<>();
 
-        for(int[] arr : descriptions){
-            int parent = arr[0], child = arr[1];
-            int isLeft = arr[2];
+        for (int[] desc : descriptions) {
+            int parent = desc[0];
+            int child = desc[1];
+            int isLeft = desc[2];
+
+            map.putIfAbsent(parent, new TreeNode(parent));
+            map.putIfAbsent(child, new TreeNode(child));
+
+            TreeNode parentNode = map.get(parent);
+            TreeNode childNode = map.get(child);
+
+            if (isLeft == 1) {
+                parentNode.left = childNode;
+            } else {
+                parentNode.right = childNode;
+            }
 
             children.add(child);
-
-            TreeNode parentNode=  map.computeIfAbsent(parent, k-> new TreeNode(parent));
-            TreeNode childNode = map.computeIfAbsent(child, k-> new TreeNode(child));
-
-            if(isLeft==1) parentNode.left = childNode;
-            else parentNode.right = childNode; 
         }
 
-        for(Integer node: map.keySet()){
-            if(!children.contains(node)) return map.get(node);
+        for (int val : map.keySet()) {
+            if (!children.contains(val)) {
+                return map.get(val);
+            }
         }
 
         return null;
     }
 }
+
+// [[20,15,1],[20,17,0],[50,20,1],[50,80,0],[80,19,1]]
+//   i
+
+// [20, 15, 1] create node if its not created prior, if created update the children 
+// [50, 20, 1] if 50 is present update children else create node check if the child node is created and update the link with existing node 
+// [50, 80, 0] create right child of 50 
+// [80, 19, 1] check if 80 node is there yes then update the children of the 80 node
