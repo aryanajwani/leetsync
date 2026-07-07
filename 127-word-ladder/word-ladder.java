d865acd7eb385dbb3dc60409125953eed4c759a2
Aryan Ajwani <aryan.ajwani@gmail.com>
2026-07-08 03:02:25 +0530

    Time: 226 ms (14.21%) | Memory: 54.5 MB (7.52%) - LeetSync

---
+++ 127-word-ladder/word-ladder.java
@@ -0,0 +1,48 @@
+class Pair{
+    String word; 
+    int level;
+
+    Pair(String word, int level){
+        this.word = word;
+        this.level =level;
+    }
+}
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> set = new HashSet<>();
+        for(String word : wordList) set.add(word);
+
+        Queue<Pair> queue = new ArrayDeque<>();
+        queue.add(new Pair(beginWord, 0));
+
+        int n = beginWord.length();
+
+        while(!queue.isEmpty()){
+            Pair node = queue.remove();
+            String word = node.word;
+            int level = node.level;
+
+            System.out.println(word +" "+level);
+
+
+            for(int i=0; i<n; i++){
+                String prev = word.substring(0, i);
+                String next = word.substring(i+1);
+
+                for(int j=0; j<26; j++){
+                    char chr = (char)(j+'a');
+                    String nWord = prev+ chr+ next;
+
+                    if(set.contains(nWord)){
+                        if(nWord.equals(endWord)) return level+2;
+                        
+                        queue.add(new Pair(nWord, level+1));
+                        set.remove(nWord);
+                    }
+                }  
+            }
+        }
+
+        return 0;
+    }
+}
