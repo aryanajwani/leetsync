6bdaf1a6950a9f41e0c17dcaae5bca60cf0d53c3
Aryan Ajwani <aryan.ajwani@gmail.com>
2026-07-08 03:31:11 +0530

    Time: 74 ms (61.65%) | Memory: 48.2 MB (50.82%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,44 +1,49 @@
-class Pair{
-    String word; 
-    int level;
+// class Pair{
+//     String word; 
+//     int level;
 
-    Pair(String word, int level){
-        this.word = word;
-        this.level =level;
-    }
-}
+//     Pair(String word, int level){
+//         this.word = word;
+//         this.level =level;
+//     }
+// }
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         Set<String> set = new HashSet<>();
         for(String word : wordList) set.add(word);
 
-        Queue<Pair> queue = new ArrayDeque<>();
-        queue.add(new Pair(beginWord, 0));
+        Queue<String> queue = new ArrayDeque<>();
+        queue.add(beginWord);
+        set.remove(beginWord);
 
         int n = beginWord.length();
+        int level=0;
 
         while(!queue.isEmpty()){
-            Pair node = queue.remove();
-            String word = node.word;
-            int level = node.level;
+            int size = queue.size();
+            
+            for(int k=0; k<size; k++){
+                String word = queue.remove();
 
-            if(word.equals(endWord)) return level+1;
+                if(word.equals(endWord)) return level+1;
 
-            char[] charArray = word.toCharArray();
-            for(int i=0; i<n; i++){
-                char old = charArray[i];
+                char[] charArray = word.toCharArray();
+                for(int i=0; i<n; i++){
+                    char old = charArray[i];
 
-                for(char chr= 'a'; chr<='z'; chr++){
-                    charArray[i] = chr;
-                    String nWord = new String(charArray);
+                    for(char chr= 'a'; chr<='z'; chr++){
+                        charArray[i] = chr;
+                        String nWord = new String(charArray);
 
-                    if(set.contains(nWord)){
-                        queue.add(new Pair(nWord, level+1));
-                        set.remove(nWord);
+                        if(set.contains(nWord)){
+                            queue.add(nWord);
+                            set.remove(nWord);
+                        }
                     }
+                    charArray[i] = old;  
                 }
-                charArray[i] = old;  
             }
+            ++level;
         }
 
         return 0;
