ac65ab5f2c72e00993a62b1ebb3f9ae2056abb6f
Aryan Ajwani <aryan.ajwani@gmail.com>
2026-08-02 22:25:36 +0530

    Time: 2 ms (41.68%) | Memory: 47.2 MB (75.17%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,28 +1,57 @@
 class Solution {
-    public int findCircleNum(int[][] adj) {
-         int V = adj.length;
-        int visited[] = new int[V+1]; 
-        int count=0;
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        DisjointSet dset = new DisjointSet(n);
 
-        for(int i=1; i<=V; i++){
-            if(visited[i]!=1){
-                count++;
-                dfs(i, visited, adj);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(isConnected[i][j] == 1 ) dset.unionBySize(i+1, j+1);
             }
+        }
+
+        int count=0;
+        for(int i=1; i<=n; i++){
+            if(dset.getUParent(i) == i) count++;
         }
 
         return count;
     }
+}
 
-    void dfs(int node, int visited[], int adj[][]){
-        visited[node] = 1;
+class DisjointSet{
+    int[] parent;
+    int[] size;
 
-        int neighbours[] = adj[node-1];
+    public DisjointSet(int n){
+        parent = new int[n+1];
+        size = new int[n+1];
 
-        for(int i=0; i<neighbours.length; i++){
-            if(neighbours[i]==1 && visited[i+1]==0){
-                dfs(i+1, visited, adj);
-            }
+        for(int i=0; i<=n; i++){
+            parent[i] = i;
+            size[i] = 1;
         }
+    }
+
+    public void unionBySize(int u, int v){
+        int uParent_u = getUParent(u);
+        int uParent_v = getUParent(v);
+
+        if(uParent_u == uParent_v) return;
+
+        if(size[uParent_u] < size[uParent_v]){
+            parent[uParent_u] = uParent_v;
+            size[uParent_v] += size[uParent_u];
+        }
+        else{
+            parent[uParent_v] = uParent_u;
+            size[uParent_u] += size[uParent_v];
+        }
+    }
+
+    public int getUParent(int node){
+        if(parent[node] == node) return node;
+
+        parent[node] = getUParent(parent[node]);
+        return parent[node];
     }
 }
