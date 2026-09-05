bd5b140acd503fd6fe192c17660f7d097d84e25d
Aryan Ajwani <aryan.ajwani@gmail.com>
2026-09-06 03:44:48 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (86.71%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -3,14 +3,23 @@ class Solution {
         int[] dp = new int[n+1];
         Arrays.fill(dp, -1);
 
-        return ways(n, dp);
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+
+        return dp[n];
+
+        // return ways(n, dp);
     }
 
-    int ways(int i, int[] dp){
-        if(i==1 || i==0) return 1;
+    // int ways(int i, int[] dp){
+    //     if(i==1 || i==0) return 1;
 
-        if(dp[i]!=-1) return dp[i];
+    //     if(dp[i]!=-1) return dp[i];
 
-        return dp[i] = ways(i-1, dp) + ways(i-2, dp);
-    }
+    //     return dp[i] = ways(i-1, dp) + ways(i-2, dp);
+    // }
 }
