7b98fff6b355ea7d56edd32fedc67fd06e0b4eaa
Aryan Ajwani <aryan.ajwani@gmail.com>
2026-09-07 20:23:10 +0530

    Time: 17 ms (5.25%) | Memory: 47 MB (39.54%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,42 +1,30 @@
 class Solution {
     public int[] findOrder(int n, int[][] prerequisites) {
-        List<Integer>[] adj = new ArrayList[n];
-        int[] indegree = new int[n];
-
-        for(int[] edge: prerequisites){
-            int u = edge[1];
-            int v = edge[0];
-
-            if(adj[u] == null) adj[u] = new ArrayList<Integer>();
-
-            adj[u].add(v);
-            ++indegree[v];
+        int indegree[] = new int[n];
+        for(int[] edge : prerequisites){
+            indegree[edge[0]]++;
         }
 
         Queue<Integer> queue = new ArrayDeque<>();
         for(int i=0; i<n; i++) if(indegree[i] ==0) queue.add(i);
 
-        return getTopoOrder(adj, indegree, queue, n);
-    }
-
-    int[] getTopoOrder(List<Integer>[] adj, int[] indegree, Queue<Integer> queue, int n){
-        int ordering[]= new int[n];
-        int index =0;
+        int[] result= new int[n];
+        int index=0;
 
         while(!queue.isEmpty()){
             int node = queue.remove();
 
-            ordering[index++] = node;
+            result[index++] = node;
 
-            if(adj[node] == null) continue;
-            
-            for(int neighbour: adj[node]){
-                if(--indegree[neighbour] == 0) queue.add(neighbour);
-            } 
+            for(int[] edge : prerequisites){
+                if(edge[1]== node){
+                    if (--indegree[edge[0]] ==0) queue.add(edge[0]);
+                }
+            }
         }
 
         if(index<n) return new int[0];
 
-        return ordering;
+        return result;
     }
 }
