78f8668db5b485fb8f0254ce585220ce05a91c13
Aryan Ajwani <aryan.ajwani@gmail.com>
2026-09-13 23:49:53 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (20.18%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,27 +1,23 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        Set<List<Integer>> set = new HashSet<>();
         List<List<Integer>> result = new ArrayList<>();
 
-        solve(0, nums, new ArrayList<>(), result, set);
+        solve(0, nums, new ArrayList<>(), result);
 
         return result;
     }
 
-    void solve(int index, int[] nums, List<Integer> list, List<List<Integer>> result, Set<List<Integer>> set){
+    void solve(int index, int[] nums, List<Integer> list, List<List<Integer>> result){
 
         if(index==nums.length){
-            if(!set.contains(list)){
-                set.add(new ArrayList(list));
-                result.add(new ArrayList(list));
-            }
+            result.add(new ArrayList(list));
             return;
         }
 
         list.add(nums[index]);
-        solve(index+1, nums, list, result, set);
+        solve(index+1, nums, list, result);
 
         list.removeLast();
-        solve(index+1, nums, list, result, set);
+        solve(index+1, nums, list, result);
     }
 }
