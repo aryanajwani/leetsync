d3ce3dfa48f0888e912f4041dc3c14da72389e6e
Aryan Ajwani <aryan.ajwani@gmail.com>
2026-09-23 20:20:09 +0530

    Time: 6 ms (58.82%) | Memory: 47.5 MB (9.04%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -3,41 +3,41 @@ class Solution {
         int m = matrix[0].length;
         int n = matrix.length;
 
-        int dp[][] = new int[n][m];
-        for(int i=0; i<n; i++) Arrays.fill(dp[i], Integer.MAX_VALUE);
+        int next[] = new int[m];
+        int current[] = new int[m];
+
+        for(int j=0; j<m; j++) next[j] = matrix[n-1][j];
+
+        for(int i=n-2; i>=0; i--){
+            for(int j=0; j<m; j++){
+
+                int middle = next[j];
+
+                int left = Integer.MAX_VALUE;
+                if(j>0) left = next[j-1];
+  
+                int right =  Integer.MAX_VALUE;
+                if(j<m-1) right = next[j+1];
+
+                current[j] = matrix[i][j] + Math.min(left, Math.min(middle, right));
+            }
+
+            int[] temp = next;
+            next = current;
+            current = temp;
+        }
 
         int min = Integer.MAX_VALUE;
         for(int i=0; i<m; i++){
-            min = Math.min(min, minFrom(0, i, matrix, dp));
+            min = Math.min(min, next[i]);
         }
 
         return min;
     }
 
-    int minFrom(int i, int j, int[][] matrix, int[][] dp){
-        if(i==matrix.length-1) return matrix[i][j];
+    // boolean isValid(int j, int[][] matrix){
+    //     int m= matrix[0].length;
 
-        if(dp[i][j] != Integer.MAX_VALUE) return dp[i][j];
-
-        int left = Integer.MAX_VALUE;
-        if(isValid(j-1, matrix)) left = minFrom(i+1, j-1, matrix, dp);
-
-        int middle = Integer.MAX_VALUE;
-        if(isValid(j, matrix)) middle = minFrom(i+1, j, matrix, dp);
-        
-        int right =  Integer.MAX_VALUE;
-        if(isValid(j+1, matrix)) right = minFrom(i+1, j+1, matrix, dp);
-
-        return dp[i][j] = matrix[i][j] + Math.min(left, Math.min(middle, right));
-    }
-
-    boolean isValid(int j, int[][] matrix){
-        int m= matrix[0].length;
-
-        return j>=0 && j<m;
-    }
+    //     return j>=0 && j<m;
+    // }
 }
-
-// [2,1,3],
-// [6,5,4],
-// [7,8,9]
